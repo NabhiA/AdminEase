b1c1c2d5cb48b9866f574f68e04eec8a052dc883
Nabhi Agrawal <nabhi.naresh2020@vitbhopal.ac.in>
2023-04-12 20:40:38 +0530

    Update dummy.js

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -172,8 +172,8 @@ export const barPrimaryYAxis = {
 };
 const areaChartData = [
   [
-    { x: new Date(2002, 0, 1), y: 2.2 },
-    { x: new Date(2003, 0, 1), y: 3.4 },
+    { x: new Date(2001, 0, 1), y: 2.2 },
+    { x: new Date(2002, 0, 1), y: 3.4 },
     { x: new Date(2004, 0, 1), y: 2.8 },
     { x: new Date(2005, 0, 1), y: 1.6 },
     { x: new Date(2006, 0, 1), y: 2.3 },
